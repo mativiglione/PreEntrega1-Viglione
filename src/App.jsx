155e478421c5e9d2bc31d79e9b8fe93491eedc23
Matias Viglione <mativiglione22@gmail.com>
2023-03-06 18:41:25 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
